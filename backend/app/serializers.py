7a4ed7bca42ca6603a929c72d60be70b57649cfd
No-Hex <evilheroe1@gmail.com>
2023-02-09 13:33:50 -0400

    Initial

---
+++ backend/app/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+
+class ContactSerializer(serializers.Serializer):
+    pass
---
+++ backend/app/views.py
@@ -0,0 +1,7 @@
+import json
+from rest_framework.response import Response
+from .serializers import ContactSerializer
+# Create your views here.
+
+def Contactslist(request):
+    pass
